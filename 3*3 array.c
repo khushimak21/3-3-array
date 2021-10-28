aa87d03cb477505ac0d2efada53db6a93b7e3265
khushimak21 <92927286+khushimak21@users.noreply.github.com>
2021-10-28 22:41:39 +0530

    Create 3*3 array.c

---
+++ 3*3 array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int array[3][3],i,j;
+printf("\n\nRead a 2D array of size 3*3 and print the matrix:\n");
+printf("\n\nRead a 2D array of size 3*3 and print the matrix:\n");
+printf("input elements is the matrix:\n");
+for(i=0;i<3;i++)
+{
+for(j=0;j<3;j++)
+{
+printf("element-[%d],[%d"]:",i,j);
+scanf("%d",array[i][j]);
+}
+}
+printf("\nThe matrix is:\n");
+for(i=0;i<3;i++)
+{
+printf("\n");
+for(j=0;j<3;J++)
+printf("%d\t",array[i][j]);
+}
+printf("\n\n");
+return o;
+}
+
